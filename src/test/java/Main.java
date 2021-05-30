8fc90e78a60cf66136eb3e0cb39e3eb4e7e3c6f8
Volcomix <Volcomix@users.noreply.github.com>
2021-05-30 19:43:44 +0200

    Run Rust Volcomix agent

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,13 +2,14 @@ import com.codingame.gameengine.runner.MultiplayerGameRunner;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        System.setProperty("league.level", String.valueOf(2));
         MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
+        gameRunner.addAgent("../tic-tac-toe-rust/target/release/tic-tac-toe-rust", "Volcomix",
+                "https://static.codingame.com/servlet/fileservlet?id=9274241205710&format=agent_avatar");
         gameRunner.addAgent(Player1.class);
-        gameRunner.addAgent(Player2.class);
-        
-        // gameRunner.addAgent("python3 /home/user/player.py");
-        
+        // gameRunner.addAgent(Player2.class);
+
         gameRunner.start();
     }
 }
